d5bcde68d8017321e1da4e62a7e3bb2c55c1335d
Thiago Yasunaka <thiagoyasunaka@hotmail.com>
2021-09-09 07:36:45 -0300

    Inicia mestre-trabalhador e implementa fila de tarefas

--- Makefile
+++ Makefile
@@ -4,13 +4,13 @@ sum: sum.c
 	gcc -g -O2 --std=c99 -Wall -o sum sum.c
 
 par_sum: par_sum.c
-	gcc -g -O2 --std=c99 -Wall -o par_sum par_sum.c -lpthread
+	gcc -g -O2 --std=c99 -Wall -o par_sum par_sum.c -lpthread ./utils/queue.c
 
 exec-sum: sum
 	./sum ./examples/simple.txt
 
 exec-par-sum: par_sum
-	./par_sum ./examples/simple.txt
+	./par_sum ./examples/simple.txt -t 3 -f ./examples/simple.txt
 
 clean:
 	rm -f sum par_sum
--- par_sum.c
+++ par_sum.c
@@ -5,13 +5,32 @@
 #include <unistd.h>
 #include <ctype.h>
 #include <getopt.h>
+#include <pthread.h>
+#include "./utils/queue.h"
 
 char* path_file;
 int n_threads = -1;
+Queue* tasks;           // fila de tarefas
+pthread_t* master, * workers;
+
+void* master_production() {
+  printf("mestre\n");
+  return NULL;
+}
+
+void* workers_consumers() {
+  printf("trabalhadores\n");
+  return NULL;
+}
 
 int main(int argc, char* argv[]) {
 
   int opt;
+  char action;  //ação de uma linha do arquivo
+  long num;     //tempo de espera de uma linha do arquivo
+  FILE* file;
+
+
   while ((opt = getopt(argc, argv, "t:f:")) != -1) {
     switch (opt) {
     case 't':
@@ -30,6 +49,49 @@ int main(int argc, char* argv[]) {
     return 0;
   }
 
-  printf("string: %i %s ", n_threads, path_file);
+  tasks = malloc(sizeof(Queue));
+  create_queue(tasks);
+
+  // leitura do arquivo e inserção na fila de tafefas
+  file = fopen(path_file, "r");
+  while (fscanf(file, "%c %ld\n", &action, &num) == 2) {
+    if (action == 'p' || action == 'e') {
+      Task task;
+      task.action = action;
+      task.time = num;
+      insert_task(tasks, task);
+
+    }
+    else {
+      printf("ERROR: Unrecognized action: '%c'\n", action);
+      exit(EXIT_FAILURE);
+    }
+  }
+  fclose(file);
+
+  // Criação das threads para o mestre e para os trabalhadores
+  master = malloc(sizeof(pthread_t));
+  workers = malloc(n_threads * sizeof(pthread_t));
+
+  pthread_create(master, NULL, (void*)master_production, NULL);
+
+  for (int i = 0; i < n_threads; i++) {
+    pthread_create(&workers[i], NULL, (void*)workers_consumers, NULL);
+  }
+
+  pthread_join(*master, NULL);
+
+  for (int i = 0; i < n_threads; i++) {
+    pthread_join(workers[i], NULL);
+  }
+
+
+  printf("SIZEOF: %i\n", queue_size(tasks));
+  print_queue(tasks);
+
+  free(tasks);
+  free(workers);
+  free(master);
+
   return 0;
 }
